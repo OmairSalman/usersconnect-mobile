fcdfc525a7492c7881d25fe44c4a94d8e9970451
Omair Salman <omairsalman@outlook.com>
2026-05-15 20:12:59 +0000

    fix: update app name in capacitor config and rename dev script to serve in package.json

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.cloudomair.usersconnect',
-  appName: 'usersconnect-mobile',
+  appName: 'UsersConnect',
   webDir: 'www'
 };
 
